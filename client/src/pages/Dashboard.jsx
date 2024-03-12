750d4fd2a3fa70fcb87d8e037e38077545ebe4a3
Abhishek65a <atupsundar3@gmail.com>
2024-03-12 23:18:06 +0530

    sidebar in progress

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSideBar from "../components/DashSideBar";
+import DashProfile from "../components/DashProfile";
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if(tabFromUrl)
+    {
+      setTab(tabFromUrl)
+    }
+  }, [location.search]);
+  return <div>
+    <div className="">
+  {/* Sidebar */}
+  <DashSideBar/>
+    </div>
+    {/*profile..*/}
+    {tab === 'profile' && <DashProfile/>}
+  </div>;
 }
