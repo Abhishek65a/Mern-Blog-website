f0ba438b4eded3d34e50c6db0a6f9cbdcae1ca94
Abhishek65a <atupsundar3@gmail.com>
2024-03-18 18:13:31 +0530

    Update user api route completed

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from './routes/user.route.js'
 import authRoutes from "./routes/auth.route.js";
+import cookieParser  from "cookie-parser";
+
 
 //configuring dotenv file
 dotenv.config()
@@ -11,8 +13,9 @@ dotenv.config()
 const app = express();
 
 //this is to get the data in json format
-app.use(express.json())
+app.use(express.json());
 
+app.use(cookieParser());
 
 //mongoDB connection code
 mongoose.connect(
