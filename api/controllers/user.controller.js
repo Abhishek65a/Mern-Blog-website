5f0ef42ecdc30100df5d565c406e341ed1930c36
Abhishek65a <atupsundar3@gmail.com>
2024-03-24 22:29:36 +0530

    added delete user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -11,7 +11,7 @@ export const updateUser = async (req, res, next) => {
     return next(errorHandler(403, "You are not allowed to update this user"));
   }
   if (req.body.password) {
-    if (req.body.password.length < 6) { 
+    if (req.body.password.length < 6) {
       return next(errorHandler(400, "Password must be atleast 6 characters"));
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -21,8 +21,8 @@ export const updateUser = async (req, res, next) => {
       return next(
         errorHandler(400, "Username must be between 7 to 20 characters")
       );
-    } 
-    if (req.body.username.includes(" ")) {
+    }
+    if (req.body.username.includes(" ")) { 
       return next(errorHandler(400, "Username must not contain spaces"));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
@@ -34,22 +34,36 @@ export const updateUser = async (req, res, next) => {
       );
     }
   }
-    try {
-      const updateUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            profilePicture: req.body.profilePicture,
-            password: req.body.password,
-          },
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
         },
-        { new: true }
-      );
-      const { password, ...rest } = updateUser._doc;
-      res.status(200).json(rest);
-    } catch (error) {
-      next(error);
-    }
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
+
+
+export const deleteUser = async (req,res,next)=>{
+    if(req.user.id !== req.params.userId){
+      return next(errorHandler(403,'You are not allowed to delete this user'))
+    }
+    try {
+      await User.findByIdAndDelete(req.params.userId);
+      res.status(200).json('User has been deleted')
+    } catch (error) {
+      next(error)
+      
+    }
+}
