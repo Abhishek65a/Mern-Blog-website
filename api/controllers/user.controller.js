dc0c7922e4f876b9c173e018aeec9ca4d637ede0
Abhishek65a <atupsundar3@gmail.com>
2024-03-24 22:31:09 +0530

    added delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -22,7 +22,7 @@ export const updateUser = async (req, res, next) => {
         errorHandler(400, "Username must be between 7 to 20 characters")
       );
     }
-    if (req.body.username.includes(" ")) { 
+    if (req.body.username.includes(" ")) {
       return next(errorHandler(400, "Username must not contain spaces"));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
@@ -54,16 +54,14 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-
-export const deleteUser = async (req,res,next)=>{
-    if(req.user.id !== req.params.userId){
-      return next(errorHandler(403,'You are not allowed to delete this user'))
-    }
-    try {
-      await User.findByIdAndDelete(req.params.userId);
-      res.status(200).json('User has been deleted')
-    } catch (error) {
-      next(error)
-      
-    }
-}
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this user"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
