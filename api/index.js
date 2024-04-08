1d00f6c55179a0870a6eeaba55fe30b2732d0966
Abhishek65a <atupsundar3@gmail.com>
2024-04-08 23:58:16 +0530

    completed upload post image functionality

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from './routes/post.route.js';
 import cookieParser  from "cookie-parser";
 
 
@@ -16,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
-
+ 
 //mongoDB connection code
 mongoose.connect(
     process.env.MONGO
@@ -29,6 +30,7 @@ mongoose.connect(
 //Base URL's
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
+app.use('/api/post',postRoutes) 
   
 
 //error handling middleware
